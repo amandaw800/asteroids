f94a2a93a9cfc3795b31c1206cf58d859f15a816
amandaw800 <amanda.v.wei@gmail.com>
2025-08-08 17:04:33 -0400

    Added player.py

--- main.py
+++ main.py
@@ -1,18 +1,48 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
 
+    #FPS
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
+    #Drawing the player
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
+    #Exit using x button
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0))
+
+    #Filling the screen and drawing the player
+        screen.fill((0,0,0))
+        player.draw(screen)
+
+        player.update(dt)
+
+    
+
+
+
+
+    #Refreshes the page
         pygame.display.flip()
+
+
+
+    # Cap at 60 fps
+        player.update(dt)
+        delta = clock.tick(60)
+
+        dt = delta / 1000
+
 
 
 if __name__ == "__main__":
